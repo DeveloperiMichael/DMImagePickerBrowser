19815f52324baf94e3616ee45ef6262660e2fce8
张炯 <michael.zhang@wwwarehouse.com>
2017-07-06 10:33:21 +0800

    Initial Commit

---
+++ DMImagePickerBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DMImagePickerBrowser
+//
+//  Created by 张炯 on 2017/7/6.
+//  Copyright © 2017年 张炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DMImagePickerBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DMImagePickerBrowser
+//
+//  Created by 张炯 on 2017/7/6.
+//  Copyright © 2017年 张炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
